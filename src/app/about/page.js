d186b8ae6477feb5a8f0f7ee502100566c4331f3
joyhasan08 <rubayedhasan235@gmail.com>
2024-03-12 19:29:10 +0600

    init

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -10,6 +10,13 @@ const AboutPage = () => {
             <BreadCrumbeAbout />
             <BannerAbout />
             <AboutTeam />
+            <div>
+                <p>
+                    This section IS under Development.
+
+                </p>
+                <p>Tempor dolor incididunt anim pariatur pariatur Lorem commodo veniam sunt tempor dolore excepteur fugiat. Consequat incididunt ea adipisicing commodo pariatur tempor excepteur ad minim est nisi laboris voluptate id. Velit id deserunt ad proident minim laboris eu fugiat proident laborum eiusmod. Adipisicing labore incididunt sit ullamco ipsum in nulla consequat. Culpa tempor do ullamco ex officia elit qui eu esse tempor anim sit.</p>
+            </div>
         </div>
     );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,12 @@
 import Business from "@/components/Business Goals/Business";
 import BusinessGoal from "@/components/Business Goals/BusinessGoal";
 import Carousel from "@/components/Carousel/Carousel";
-import Footer from "@/components/Footer/Footer";
+
 import Stats from "@/components/Stats/Stats";
 import StepsSection from "@/components/Steps/StepsSection";
 import TeamSection from "@/components/Team/TeamSection";
 import Testimonial from "@/components/Testimonial/Testimonial";
-import Image from "next/image";
-import SubMenu from "@/components/SubMenu/SubMenu";
+
 import StepsFull from "@/components/Steps/StepsFull";
 
 export default function Home() {
